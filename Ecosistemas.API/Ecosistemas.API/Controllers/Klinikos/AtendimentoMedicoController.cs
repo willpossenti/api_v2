ae130c4b3f0f31242520c42ab8f3e5e0c4f88721
GitEcoSistemas <47902145+GitEcoSistemas@users.noreply.github.com>
2019-06-05 16:58:17 -0300

    enviar

---
+++ Ecosistemas.API/Ecosistemas.API/Controllers/Klinikos/AtendimentoMedicoController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Ecosistemas.Business;
+using Ecosistemas.Business.Entities.Klinikos;
+using Ecosistemas.Business.Contexto.Klinikos;
+using Ecosistemas.Business.Interfaces.Klinikos;
+using Ecosistemas.Business.Services.Klinikos;
+using Ecosistemas.Security.Manager;
+using Ecosistemas.Business.Utility;
+using Ecosistemas.Business.Contexto.Api;
+
+namespace Ecosistemas.API.Controllers.Klinikos
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize("Bearer")]
+    public class AtendimentoMedicoController : Controller
+    {
+        private readonly IAtendimentoMedicoService _service;
+
+        public AtendimentoMedicoController(KlinikosDbContext contextKlinikos, ApiDbContext context)
+        {
+            _service = new AtendimentoMedicoService(contextKlinikos, context);
+        }
+
+        [Route("Incluir")]
+        [HttpPost]
+        [Authorize(Roles = "" + Roles.ROLE_API_MASTER + "," + Roles.ROLE_API_KLINIKOS + "")]
+        public async Task<CustomResponse<AtendimentoMedico>> Incluir([FromBody]AtendimentoMedico atendimentoMedico)
+        {
+            return await _service.AdicionarAtendimentoMedico(atendimentoMedico, Guid.Parse("B9AB33C3-6697-49F4-BF30-598214D0B7F2"));
+        }
+
+        [HttpPut]
+        [Authorize(Roles = "" + Roles.ROLE_API_MASTER + "," + Roles.ROLE_API_KLINIKOS + "")]
+        public async Task<CustomResponse<AtendimentoMedico>> Put([FromBody]AtendimentoMedico atendimentoMedico, [FromServices]AccessManager accessManager)
+        {
+            return await _service.Atualizar(atendimentoMedico, Guid.Parse(HttpContext.User.Identity.Name));
+        }
+
+
+        [HttpDelete("{AtendimentoMedicoId}")]
+        [Authorize(Roles = "" + Roles.ROLE_API_MASTER + "," + Roles.ROLE_API_KLINIKOS + "")]
+        public async Task<CustomResponse<AtendimentoMedico>> Delete(string AtendimentoMedicoId)
+        {
+            return await _service.Remover(Guid.Parse(AtendimentoMedicoId), Guid.Parse(HttpContext.User.Identity.Name));
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "" + Roles.ROLE_API_MASTER + "," + Roles.ROLE_API_KLINIKOS + "")]
+        public async Task<CustomResponse<IList<AtendimentoMedico>>> Get()
+        {
+            return await _service.ListarTodos();
+        }
+
+        [HttpGet("{AtendimentoMedicoId}")]
+        [Authorize(Roles = "" + Roles.ROLE_API_MASTER + "," + Roles.ROLE_API_KLINIKOS + "")]
+        public async Task<CustomResponse<AtendimentoMedico>> Get(string AtendimentoMedicoId)
+        {
+            return await _service.Obter(Guid.Parse(AtendimentoMedicoId));
+        }
+
+
+
+    }
+}
---
+++ Ecosistemas.API/Ecosistemas.API/Controllers/Klinikos/AtendimentoMedicoExameController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Ecosistemas.Business;
+using Ecosistemas.Business.Entities.Klinikos;
+using Ecosistemas.Business.Contexto.Klinikos;
+using Ecosistemas.Business.Interfaces.Klinikos;
+using Ecosistemas.Business.Services.Klinikos;
+using Ecosistemas.Security.Manager;
+using Ecosistemas.Business.Utility;
+using Ecosistemas.Business.Contexto.Api;
+
+namespace Ecosistemas.API.Controllers.Klinikos
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AtendimentoMedicoExameController : Controller
+    {
+        private readonly IAtendimentoMedicoExameService _service;
+
+        public AtendimentoMedicoExameController(KlinikosDbContext contextKlinikos, ApiDbContext context)
+        {
+            _service = new AtendimentoMedicoExameService(contextKlinikos, context);
+        }
+
+        [Route("Incluir")]
+        [HttpPost]
+        //[Authorize(Roles = "" + Roles.ROLE_API_MASTER + "," + Roles.ROLE_API_KLINIKOS + "")]
+        public async Task<CustomResponse<AtendimentoMedicoExame>> Incluir([FromBody]AtendimentoMedicoExame atendimentoMedicoExame)
+        {
+            return await _service.AdicionarAtendimentoMedicoExame(atendimentoMedicoExame, Guid.Parse("B9AB33C3-6697-49F4-BF30-598214D0B7F2"));
+        }
+
+        [HttpPut]
+        //[Authorize(Roles = "" + Roles.ROLE_API_MASTER + "," + Roles.ROLE_API_KLINIKOS + "")]
+        public async Task<CustomResponse<AtendimentoMedicoExame>> Put([FromBody]AtendimentoMedicoExame atendimentoMedicoExame, [FromServices]AccessManager accessManager)
+        {
+            return await _service.Atualizar(atendimentoMedicoExame, Guid.Parse(HttpContext.User.Identity.Name));
+        }
+
+
+        [HttpDelete("{AtendimentoMedicoExameId}")]
+        //[Authorize(Roles = "" + Roles.ROLE_API_MASTER + "," + Roles.ROLE_API_KLINIKOS + "")]
+        public async Task<CustomResponse<AtendimentoMedicoExame>> Delete(string AtendimentoMedicoExameId)
+        {
+            return await _service.Remover(Guid.Parse(AtendimentoMedicoExameId), Guid.Parse(HttpContext.User.Identity.Name));
+        }
+
+        [HttpGet]
+        //[Authorize(Roles = "" + Roles.ROLE_API_MASTER + "," + Roles.ROLE_API_KLINIKOS + "")]
+        public async Task<CustomResponse<IList<AtendimentoMedicoExame>>> Get()
+        {
+            return await _service.ListarTodos();
+        }
+
+        [HttpGet("{AtendimentoMedicoExameId}")]
+        [Authorize(Roles = "" + Roles.ROLE_API_MASTER + "," + Roles.ROLE_API_KLINIKOS + "")]
+        public async Task<CustomResponse<AtendimentoMedicoExame>> Get(string AtendimentoMedicoExameId)
+        {
+            return await _service.Obter(Guid.Parse(AtendimentoMedicoExameId));
+        }
+
+
+
+    }
+}
